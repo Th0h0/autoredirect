32e68a153fe934cae7bafb66ec46ef343f042780
Th0h0 <thomas.houhou@gmail.com>
2022-02-28 17:17:13 +0100

    fix bug

--- autoredirect.py
+++ autoredirect.py
@@ -95,9 +95,13 @@ def main():
         except:
             print("\nInvalid URL")
     elif args.file:
-        for url in args.file:
+
+        targetURLS = open(args.file, "r")
+
+        for url in targetURLS:
             try:
                 fuzz_open_redirect(url)
             except:
                 continue
+        targetURLS.close()
 main()
