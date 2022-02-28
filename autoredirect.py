f1a33e08b1e2cd0caa2aed019baeef16c654b2a5
Th0h0 <thomas.houhou@gmail.com>
2022-02-28 18:17:41 +0100

    Improvements : -oneshot mode (incompatible with -smart mode) -add more clarity to each url fuzzing on verbose mode

--- autoredirect.py
+++ autoredirect.py
@@ -4,12 +4,14 @@ import requests
 
 CANARY_TEXT = 'CANARY049'
 CANARY_DOMAIN = 'canaryredirect.fr'
+FUZZ_PLACE_HOLDER = 'FUZZ-HERE'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", "-f", type=str, required=False, help= 'file of all URLs to be tested against Open Redirect')
 parser.add_argument("--url", "-u", type=str, required=False, help= 'url to be tested against Open Redirect.')
 parser.add_argument("--verbose", "-v", action='store_true', help='activate verbose mode for the tool')
 parser.add_argument("--smart", "-s", action='store_true', help='activate context-based payload generation for each tested URL')
+parser.add_argument("--oneshot", "-t", action='store_true', help='fuzz with only one basic payload - to be activated in case of time constraints')
 parser.add_argument("--output", "-o", action='store_true', help='output file path')
 
 args = parser.parse_args()
@@ -17,8 +19,15 @@ args = parser.parse_args()
 if not (args.file or args.url):
     parser.error('No input selected: Please add --file or --url as arguments.')
 
+if args.smart and args.oneshot:
+    parser.error('Incompatible modes chosen : oneshot mode implies that only one payload is used.')
+
 defaultPayloadFile = open("default-payloads.txt", "r")
-payloads = [payload.replace('\n', '') for payload in defaultPayloadFile]
+
+if args.oneshot:
+    payloads = [f"http://{CANARY_DOMAIN}"]
+else:
+    payloads = [payload.replace('\n', '') for payload in defaultPayloadFile]
 
 regexMultipleParams = '(?<=(Url|URL|Open|callback|checkout|continue|data|dest|destination|dir|domain|feed|file|file_name|folder|forward|go|goto|host|html|load_file|login\?to|logout|navigation|next|next_page|out|page|path|port|redir|redirect|redirect_to|uri|URI|Uri|reference|return|returnTo|return_path|return_to|rt|show|site|target|to|url|val|validate|view|window)=)(.*)(?=&)'
 
@@ -67,9 +76,10 @@ def fuzz_open_redirect(url, payloadsList = payloads):
         host = smart_extract_host(url , matchedElem)
         payloadsList += generate_payloads(host)
 
-    url = url.replace(matchedElem, "???")
+    url = url.replace(matchedElem, FUZZ_PLACE_HOLDER)
 
     if args.verbose:
+        print(f" + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +")
         print(f"Starting fuzzing {url}")
 
     for payload in payloadsList:
@@ -78,10 +88,10 @@ def fuzz_open_redirect(url, payloadsList = payloads):
             output.write(f"Open Redirect detected in {url} with payload {payload}\n")
             return
     if args.verbose:
-        print("\nNothing detected for the given URL.")
+        print("\nNothing detected for the given URL.\n")
 
 def detected_vuln_with_payload(url, payload):
-    fuzzedUrl = url.replace('???', payload)
+    fuzzedUrl = url.replace(FUZZ_PLACE_HOLDER, payload)
 
     if args.verbose:
         print(f"Testing payload: {payload}                                                          ", end="\r")
